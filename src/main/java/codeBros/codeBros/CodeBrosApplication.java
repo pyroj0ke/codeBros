5450d664f6033ef05c8bc4bfd2234905d68bcee9
xamorogonth <60507021+xamorogonth@users.noreply.github.com>
2021-03-06 20:01:30 +0300

    Add files via upload

---
+++ src/main/java/codeBros/codeBros/CodeBrosApplication.java
@@ -0,0 +1,13 @@
+package codeBros.codeBros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeBros/codeBros/CodeBrosApplicationTests.java
@@ -0,0 +1,13 @@
+package codeBros.codeBros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
